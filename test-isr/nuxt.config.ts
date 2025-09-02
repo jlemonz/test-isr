267ee99dca202449af2e23479b6ad047924db712
jlemonz <112578448+jlemonz@users.noreply.github.com>
2025-09-02 19:01:34 +0200

    isr

--- test-isr/nuxt.config.ts
+++ test-isr/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  routeRules: {
+    // all routes (by default) will be revalidated every 60 seconds, in the background
+    "/**": { isr: 60 },
+  },
+});
