ee57c782c053412bbab203f60ef083b03f7570ef
jlemonz <112578448+jlemonz@users.noreply.github.com>
2025-09-04 19:01:20 +0200

    w

--- test-isr/nuxt.config.ts
+++ test-isr/nuxt.config.ts
@@ -2,7 +2,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-14",
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   routeRules: {
