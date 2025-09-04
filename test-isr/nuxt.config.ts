fb99f01d7bd274509674241da29e079f7fe150a1
jlemonz <112578448+jlemonz@users.noreply.github.com>
2025-09-04 19:14:01 +0200

    w

--- test-isr/nuxt.config.ts
+++ test-isr/nuxt.config.ts
@@ -15,16 +15,16 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
   css: ["~/assets/css/main.css"],
-  // modules: ["@nuxtjs/supabase"],
-  // supabase: {
-  //   redirect: false,
-  //   // Options
-  //   redirectOptions: {
-  //     login: "/login",
-  //     callback: "/confirm",
-  //     include: undefined,
-  //     exclude: ["/", "/confirm"],
-  //     saveRedirectToCookie: false,
-  //   },
-  // },
+  modules: ["@nuxtjs/supabase"],
+  supabase: {
+    redirect: false,
+    // Options
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      include: undefined,
+      exclude: ["/", "/confirm"],
+      saveRedirectToCookie: false,
+    },
+  },
 });
