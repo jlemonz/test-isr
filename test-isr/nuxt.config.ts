c9e25e440dea9e1cc78c6462b8ded8c095dacf9d
jlemonz <112578448+jlemonz@users.noreply.github.com>
2025-09-04 09:01:15 +0200

    s

--- test-isr/nuxt.config.ts
+++ test-isr/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 
   routeRules: {
     // all routes (by default) will be revalidated every 60 seconds, in the background
-    "/**": { isr: 60 },
+    "/": { isr: 60 },
   },
   vite: {
     plugins: [tailwindcss()],
